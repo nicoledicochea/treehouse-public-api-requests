28203bb700f87672c41707895f7ef591d6c1bb2e
“Nicole <dicocheanicole@gmail.com>
2023-10-03 08:56:59 -0700

    update comments

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-// get and display 12 random users
-// refer to: https://randomuser.me/documentation
-
-// fetch data from randomuser.me 
-// img, first/last name, email, city or location
-// style it like html in index.html example
-
-const url = 'https://randomuser.me/api/?nat=us&results=12'
 const galleryCards = []
 const gallery = document.querySelector('#gallery')
 const body = document.querySelector('body')
 
+// get and display 12 random users with US nationality from randomuser.me 
+const url = 'https://randomuser.me/api/?nat=us&results=12'
+
+// create fetch request function
 async function fetchUsers() {
     try {
         const response = await fetch(url)
@@ -20,6 +16,7 @@ async function fetchUsers() {
     }
 }
 
+// return gallery card HTML
 function createGalleryCard(data) {
     return `<div class="card">
         <div class="card-img-container">
@@ -33,7 +30,7 @@ function createGalleryCard(data) {
     </div>`
 }
 
-
+// return modal card HTML
 function createModalCard(data) {
     return `<div class="modal-container">
         <div class="modal">
@@ -42,18 +39,20 @@ function createModalCard(data) {
             <img class="modal-img" src="${data.picture.large}" alt="${data.name.first} ${data.name.last} profile picture">
             <h3 id="${data.name.first}" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
             <p class="modal-text">${data.email}</p>
-            <p class="modal-text cap">${data.location.city},</p>
+            <p class="modal-text cap">${data.location.city}</p>
             <hr>
             <p class="modal-text">${formatPhoneNums(data.cell)}</p>
-            <p class="modal-text">123 ${data.location.street.name}, ${data.location.city}, ${data.location.state} ${data.location.postcode}</p>
+            <p class="modal-text">123 ${data.location.street.name}, ${data.location.city}, ${data.location.state}, ${data.location.postcode}</p>
             <p class="modal-text">Birthday: ${formatBirthdates(data.dob.date)}</p>
         </div>
     </div>
     </div>`
 }
 
+// add gallery cards to the DOM
 function generateGallery(data) {
     gallery.insertAdjacentHTML('beforeend', data.map(person => {
+        // add each person object to galleryCards array
         galleryCards.push(person)
         return createGalleryCard(person)
     }).join('')) 
@@ -69,22 +68,27 @@ gallery.addEventListener('click', (e) => {
             const cardName = cardClicked.querySelector('h3').id
             return cardName === person.name.first
         })
-    // create a modal window
+
+    // create modal card
     const card = createModalCard(data)
+
+    // add modal HTML to the DOM
     gallery.insertAdjacentHTML('beforebegin', card)
 
-    // create close modal event listener
+    // add close modal functionality 
     const modalContainer = document.querySelector('.modal-container')
-
     const closeButton = document.querySelector('#modal-close-btn')
+    // close modal when close button clicked
     closeButton.addEventListener('click', (e) => {
         modalContainer.remove()
     })  
+    // close modal when escape key used
     addEventListener('keydown', (e) => {
         if(e.key === 'Escape') {
             modalContainer.remove() 
         }
     })
+    // close modal when outside of modal container clicked
     addEventListener('click', (e) => {
         if(e.target.parentElement === body) {
             modalContainer.remove() 
@@ -93,24 +97,25 @@ gallery.addEventListener('click', (e) => {
     }
 })
 
+
+
+// reference for help with regex for formatting
+// Sean Bright's answer to this:
+// https://stackoverflow.com/questions/8358084/regular-expression-to-reformat-a-us-phone-number-in-javascript
+const notDigits = /[\D]+/g
+
 // format cell phone numbers
 // (xxx) xxx-xxxx
-
 function formatPhoneNums(phone) {
-    const number = phone.split('').filter(item => {
-        return Number.isInteger(+item) && item !== ' '
-    }).join('')
-    const expression = /(\d{3})(\d{3})(\d{4})/
-    return number.replace(expression, '($1) $2-$3')
+    phone = phone.replace(notDigits, '')
+    const phoneFormat = /(\d{3})(\d{3})(\d{4})/
+    return phone.replace(phoneFormat, '($1) $2-$3')
 }
-
 
 // format birthdates
 // mm/dd/yyyy
-
 function formatBirthdates(date) {
-    const removeSpaces = /\D+/g
-    date = date.replace(removeSpaces, '')
+    date = date.replace(notDigits, '')
     date = date.slice(0, 8)
     const dateFormat = /(\d{4})(\d{2})(\d{2})/
     return date.replace(dateFormat, '$2/$3/$1')
