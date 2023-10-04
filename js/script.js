d82bd9015ebb7004c2292cc8ca59da360fbcf3b8
“Nicole <dicocheanicole@gmail.com>
2023-10-04 13:20:38 -0700

    update comments and code organization

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 const users = []
 const gallery = document.querySelector('#gallery')
-const body = document.querySelector('body')
 
 // get and display 12 random users with US nationality from randomuser.me 
 const url = 'https://randomuser.me/api/?nat=us&results=12'
@@ -30,6 +29,16 @@ function createGalleryCard(data) {
     </div>`
 }
 
+// add gallery cards to the DOM
+function generateGallery(data) {
+    gallery.insertAdjacentHTML('beforeend', data.map(person => {
+        // add each person object to galleryCards array
+        if(users.length < 12) {
+            users.push(person)
+        }
+        return createGalleryCard(person)
+    }).join('')) 
+}
 
 // return modal card HTML
 function createModalCardHTML(data) {
@@ -55,10 +64,22 @@ function createModalCardHTML(data) {
     `
 }
 
+// display modal
+function displayModal(data) {
+    // create modal card
+    const card = createModalCardHTML(data)
+    // add html to the DOM
+    gallery.insertAdjacentHTML('beforebegin', card)
+}
+
+// display previous modal
 function getPrevModal() {
     const modalContainer = document.querySelector('.modal-container')
+    // get current user modal index from data-index attribute
     let currentModalIndex = modalContainer.dataset.index
+    // change string to number
     currentModalIndex = +currentModalIndex
+    // if at the beginning of the list
     if(currentModalIndex != 0) {
         const newCard = createModalCardHTML(users[currentModalIndex - 1])
         modalContainer.remove()
@@ -66,10 +87,14 @@ function getPrevModal() {
     }
 }
 
+// display next modal
 function getNextModal() {
     const modalContainer = document.querySelector('.modal-container')
+    // get current user modal index from data-index attribute
     let currentModalIndex = modalContainer.dataset.index
+    // change string to number
     currentModalIndex = +currentModalIndex
+    // if reach end of the list
     if(currentModalIndex != 11) {
         const newCard = createModalCardHTML(users[currentModalIndex + 1])
         modalContainer.remove()
@@ -77,21 +102,10 @@ function getNextModal() {
     }
 }
 
-// add gallery cards to the DOM
-function generateGallery(data) {
-    gallery.insertAdjacentHTML('beforeend', data.map(person => {
-        // add each person object to galleryCards array
-        if(users.length < 12) {
-            users.push(person)
-        }
-        return createGalleryCard(person)
-    }).join('')) 
-}
-
 // create event listener on gallery container
 gallery.addEventListener('click', (e) => {
     const cardClicked = e.target.closest('.card')
-    // ensure only gallery card selected
+    // ensure only gallery card clicked
     if(!cardClicked) {
         return
     }
@@ -100,25 +114,24 @@ gallery.addEventListener('click', (e) => {
         const cardName = cardClicked.querySelector('h3').id
         return cardName === person.name.first
     })
-    // create modal card
-    const card = createModalCardHTML(userData)
-    // add html to the DOM
-    gallery.insertAdjacentHTML('beforebegin', card)
+    // display modal
+    displayModal(userData)
 })
 
-
-// add event listeners to modal container
+// add event listeners to modal buttons
 addEventListener('click', (e) => {
     const modalContainer = document.querySelector('.modal-container')
+    // previous button
     if(e.target.closest('#modal-prev')) {
         getPrevModal()
+    // next button
     } else if (e.target.closest('#modal-next')) {
         getNextModal()
+    // close button
     } else if(e.target.closest('.modal-close-btn')) {
         modalContainer.remove()
     }
 })
-
 // close modal when escape key used
 addEventListener('keydown', (e) => {
     const modalContainer = document.querySelector('.modal-container')
@@ -129,12 +142,11 @@ addEventListener('keydown', (e) => {
 // close modal when outside of modal container clicked
 addEventListener('click', (e) => {
     const modalContainer = document.querySelector('.modal-container')
-
+    const body = document.querySelector('body')
     if(e.target.parentElement === body) {
         modalContainer.remove() 
     }
 })
-
 
 // reference for help with regex for formatting
 // Sean Bright's answer to this:
@@ -158,23 +170,17 @@ function formatBirthdates(date) {
     return date.replace(dateFormat, '$2/$3/$1')
 }
 
-// call fetchUsers()
-fetchUsers()
-
 // create and append elements needed for search bar
 const searchContainer = document.querySelector('.search-container')
-
 const searchInput = document.createElement('input')
 searchInput.type = 'search'
 searchInput.id = 'search-input'
 searchInput.classList.add('search-input')
 searchInput.placeholder = 'Search...'
-
 const searchSubmit = document.createElement('input')
 searchSubmit.type = 'submit'
 searchSubmit.id = 'search-submit'
 searchSubmit.classList.add('search-submit')
-
 searchContainer.appendChild(searchInput)
 searchContainer.appendChild(searchSubmit)
 
@@ -211,4 +217,7 @@ searchSubmit.addEventListener('click', (e) => {
     removeGalleryCards()
     searchUsers(searchInput.value)
 })
+
+// call fetchUsers()
+fetchUsers()
 
